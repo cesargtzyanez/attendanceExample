d9891154ba41307c89377fdd00e107ef532b53a8
César Gutiérrez Yáñez <cesargy@gmail.com>
2019-07-24 00:25:00 -0500

    small changes

--- backend/app/api/index.js
+++ backend/app/api/index.js
@@ -6,12 +6,7 @@ const userDB = require('../database/usersDB');
 
 const router = new Router();
 
-router.post('/login',function (req,resp) {
-  console.log('REQUEST:', req.body);
-
-});
-
-router.post('/signin', function(req, res) {
+router.post('/login', function(req, res) {
   console.log('REQUEST HERE:', req.body);
   userDB.getUserByEmail(req.body.username).then((resp) => {
         if (!resp.length) {
@@ -23,25 +18,16 @@ router.post('/signin', function(req, res) {
         console.log('USER: ', resp[0]);
         console.log('PW: ', req.body.password);
 
-        //const resPw = bcrypt.compareSync(req.body.password, resp[0].password);
-        //console.log('PW:', resPw);
-
-        /*if (resPw) {
-          res.json({success: true, token: 'JWT '});
-        } else {
-          res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
-        }*/
-
         bcrypt.compare(req.body.password, resp[0].password).then(resPw => {
             if (resPw) {
-              res.json({success: true, token: 'JWT!! '});
+              res.json({success: true});
             } else {
               res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
             }
         });
       })
       .catch((error) => {
-        console.log('wrong', error);
+        console.log('Error:', error);
         res.status(400).send(error)
       });
 });
