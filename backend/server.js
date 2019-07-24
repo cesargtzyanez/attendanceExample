66da3b00f42ecbaf3f6b97a93b0129d90b8da065
César Gutiérrez Yáñez <cesargy@gmail.com>
2019-07-24 00:25:21 -0500

    server 1st version

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const express = require('express');
+const usersRouter = require('./app/api/users');
+const api = require('./app/api');
+
+const test = require('./test');
+test();
+
+const port = 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use('/api',api);
+app.use('/',usersRouter);
+
+app.listen(port, () => console.log(`Running Attendance App on ${port}`));
